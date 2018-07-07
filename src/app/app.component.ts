f938199f2c50cf8a916f07a3f0b7ddb152255258
sawyerbutton <lil103@pitt.edu>
2018-07-07 17:38:55 +0800

    added demo for setTimeout and onpush

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import {ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit{
+  data = [{name: 'Button'}];
+  constructor(public cdr: ChangeDetectorRef) {}
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.data.push({name: 'Tommy'});
+      // setTimeout + OnPush也需要配合 markForCheck使用
+      this.cdr.markForCheck();
+    }, 2000);
+  }
 }
